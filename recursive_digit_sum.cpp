7b2ef2dce57cbcc8c83c900c6b8757932ffe752c
arwysyah <arwysyahputrasiregar@gmail.com>
2023-11-19 21:46:17 +0700

    add and solved recursive_digit_sum.cpp

---
+++ recursive_digit_sum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <istream>
+
+using namespace std;
+
+string lenSum(string n)
+{
+    int lenSum = 0;
+
+    for (char ch : n)
+    {
+        lenSum += int(ch - '0');
+    }
+
+    return to_string(lenSum);
+}
+
+int superDigitRec(string n)
+{
+    if (n.size() == 1)
+    {
+        return stoi(n);
+    }
+
+    return superDigitRec(lenSum(n));
+}
+
+int superDigit(string n, int k)
+{
+    int temp = superDigitRec(n) * k;
+
+    if (temp > 9)
+    {
+        temp = superDigitRec(to_string(temp));
+    }
+
+    return temp;
+}
+
+int main()
+{
+
+    int result = superDigit("148", 3);
+    int result2 = superDigit("9875", 4);
+    cout << result << endl;
+
+    return 0;
+}
