35dfbce4479d8d908334abef8ab4b7e345d586c8
arwysyah <arwysyahputrasiregar@gmail.com>
2023-11-25 00:06:00 +0700

    searchInsertPosition.cpp

---
+++ searchInsertPosition.cpp
@@ -0,0 +1,55 @@
+
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+
+//                                                                                                                      You must write an algorithm with
+//                                                                                                                      O(log n) runtime complexity.
+
+//                                                                                                                      Example 1 :
+
+//     Input : nums = [ 1, 3, 5, 6 ],
+//             target = 5 Output : 2 Example 2 :
+
+//     Input : nums = [ 1, 3, 5, 6 ],
+//             target = 2 Output : 1 Example 3 :
+
+//     Input : nums = [ 1, 3, 5, 6 ],
+//             target = 7 Output : 4
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int pointer = 0;
+
+        while (pointer < nums.size())
+        {
+
+            if (target > nums[pointer])
+            {
+                pointer += 1;
+            }
+            else
+            {
+                return pointer;
+            }
+        }
+        return pointer;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {1, 3, 5, 6};
+
+    Solution soultion;
+
+    int result = soultion.searchInsert(arr, 5);
+    cout << result;
+    return 0;
+}
