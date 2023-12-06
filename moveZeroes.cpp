f9d9f16f5c8e67ffa962b8387aa9e4cd50764451
arwysyah <arwysyahputrasiregar@gmail.com>
2023-12-07 02:46:45 +0700

    revert back moveZeroes

--- moveZeroes.cpp
+++ moveZeroes.cpp
@@ -1,7 +1,7 @@
 
 // Note that you must do this in-place without making a copy of the array.
 //
-//  
+//
 //
 // Example 1:
 //
@@ -11,56 +11,57 @@
 //
 // Input: nums = [0]
 // Output: [0]
-//  
+//
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-void display (vector<int> &arrs ){
-  for(auto res : arrs){
+void display(vector<int> &arrs)
+{
+  for (auto res : arrs)
+  {
     cout << res << endl;
   }
 }
-class Solution {
+class Solution
+{
 
-  public:
-  void moveZeroes(vector<int> &nums){
-  
+public:
+  void moveZeroes(vector<int> &nums)
+  {
+
+    int len = 0;
+
     int pointer = 0;
 
-    while(pointer < nums.size()-1 ){
-        if(nums[pointer] == 0){
-        nums.push_back(nums[pointer]);
-        pointer++;
-        int min = pointer -1;
-        cout << min << "Min "<< endl;
-        nums.erase(nums.begin()+ min);
-      
-
-      }else{
-        pointer++;
+    while (pointer < nums.size() - 1)
+    {
+      if (nums[len] == 0)
+      {
+        nums.push_back(nums[len]);
+        nums.erase(nums.begin() + len);
       }
+      else
+      {
+        len++;
+      }
+      pointer += 1;
     }
-    
-    
+
     display(nums);
   }
-}
-;
+};
 
-int main(){
+int main()
+{
 
-
-  Solution solution ;
-  vector<int> arr {0,1,0,3,12};
-  vector <int> arr1 {0,0,1};
-  vector <int> arrs {2,1};
+  Solution solution;
+  vector<int> arr{0, 1, 0, 3, 12};
+  vector<int> arr1{0, 0, 1};
+  vector<int> arrs{2, 1};
   solution.moveZeroes(arr1);
 
-
   return 0;
-
-
 }
