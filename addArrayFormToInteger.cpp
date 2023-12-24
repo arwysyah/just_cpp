4a76aaa7ce2ee39632230afed5455799e4a5887c
arwysyah <arwysyahputrasiregar@gmail.com>
2023-12-24 14:23:17 -0300

    add last changes problem solve

---
+++ addArrayFormToInteger.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+class Solution {
+
+public:
+    vector<int> addToArrayForm (vector<int> &num, int k){
+
+    return {};
+  }
+
+};
+
+int main (){
+ 
+  vector<int> arrA = {1,2,0,0};
+  int k  = 34;
+  Solution solution;
+  vector<int> result = solution.addToArrayForm(arrA, k);
+  cout << "Here is the result "  << endl;
+
+  return 0;
+}
