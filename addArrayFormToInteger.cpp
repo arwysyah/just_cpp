2a72810fcc5b08c526eff11cc12417201e7761f0
arwysyah <arwysyahputrasiregar@gmail.com>
2023-12-24 15:47:29 -0300

    add and update addArrayFormToInteger.cpp

--- addArrayFormToInteger.cpp
+++ addArrayFormToInteger.cpp
@@ -4,24 +4,58 @@
 using namespace std;
 
 
-
 class Solution {
 
 public:
-    vector<int> addToArrayForm (vector<int> &num, int k){
+    vector<int> addToArrayForm(std::vector<int>& num, int k) {
+    int n = num.size();
+    std::vector<int> result;
 
-    return {};
-  }
+    int carry = 0;
+    for (int i = n - 1; i >= 0 || k > 0; i--) {
+        int sum = carry;
+        if (i >= 0) {
+            sum += num[i];
+        }
+        if (k > 0) {
+            sum += k % 10;
+            k /= 10;
+        }
+
+        result.insert(result.begin(), sum % 10);
+        carry = sum / 10;
+    }
+
+    if (carry > 0) {
+        result.insert(result.begin(), carry);
+    }
+ 
+
+    return result;
+}
 
 };
 
+
+void display (vector<int> &nums){
+
+for(auto x : nums){
+    cout << x << endl;
+  }
+}
 int main (){
  
-  vector<int> arrA = {1,2,0,0};
-  int k  = 34;
+  vector<int> arrA = {3,8,0,3,0,2,7,0,7,6,4,9,9,1,7,6,6,1,6,4};
+
+  vector<int> arrB = {2,7,4};
+  int k2 = 181;
+  vector <int> arrC = {9,9,9,9,9,9,9,9,9,9};
+  int k3 = 1;
+
+  int k  = 670;
   Solution solution;
   vector<int> result = solution.addToArrayForm(arrA, k);
-  cout << "Here is the result "  << endl;
-
+  cout << "Here is the result "   << endl;
+  display(result);
   return 0;
 }
