f37f5847a9224b81189a3a1e11b11fbb94eeed2a
arwysyah <arwysyahputrasiregar@gmail.com>
2023-12-31 19:09:13 +0700

    add Generate Parenthesis.cpp

--- generateParenthesis.cpp
+++ generateParenthesis.cpp
@@ -1,28 +1,45 @@
+
+//
+//
+// Example 1:
+//
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+//
+// Example 2:
+//
+// Input: n = 1
+// Output: ["()"]
+//
+
+// LEVEL = MEDIUM
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 class Solution {
 private:
-  void helper(vector<string> result, string parenthesis, int left, int right) {
-    if (left < 0 && left > right) {
+  void createParenthesis(vector<string> &result, string parenthesis, int left,
+                         int right) {
+    if (left < 0 || left > right) {
       return;
     }
     if (left == 0 && right == 0) {
 
       result.push_back(parenthesis);
+      return;
     }
 
-    helper(result, "(", left - 1, right);
-    helper(result, ")", left, right - 1);
+    createParenthesis(result, parenthesis + "(", left - 1, right);
+    createParenthesis(result, parenthesis + ")", left, right - 1);
   }
 
 public:
   vector<string> generateParenthesis(int n) {
     vector<string> result = {};
 
-    helper(result, "", n, n);
-
+    createParenthesis(result, "", n, n);
     return result;
   }
 };
@@ -38,7 +55,6 @@ int main() {
   Solution solution;
   int nOfNumbers = 3;
   vector<string> res = solution.generateParenthesis(nOfNumbers);
-
   display(res);
 
   return 0;
