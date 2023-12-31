16c8f9843a45a9d769d8f6a026c38994434696e3
arwysyah <arwysyahputrasiregar@gmail.com>
2024-01-01 02:23:09 +0700

    add new brian kernughan algorithn

--- count_bits.cpp
+++ count_bits.cpp
@@ -6,7 +6,27 @@
 using namespace std;
 
 class Solution {
+private:
+  void display(vector <int> arr ){
+    for(auto x :arr){
+      cout << x << endl;
+    }
+  }
+    int countSetBits(int num) {
+        // Initialize a counter for counting set bits
+        int count = 0;
 
+        // Continue the loop until all set bits are counted
+        while (num > 0) {
+            // Increment the counter for each set bit
+            count++;
+
+            // Turn off the rightmost set bit
+            num = num & (num - 1);
+        }
+
+        return count;
+    };
 public:
   vector<int> countBits(int n) {
 
@@ -29,17 +49,42 @@ public:
       counter = 0;
     }
 
-    for (auto x : res) {
-      cout << x << endl;
-    }
+display(res);
+
     return res;
   }
+ vector<int> countBitsOptimumWay(int n) {
+        vector<int> res(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            // Counting set bits using Brian Kernighan's Algorithm
+            res[i] = res[i & (i - 1)] + 1;
+        }
+
+  display(res); 
+        return res;
+    }
+
+     vector<int> countBitsWithBrianKernighan(int n) {
+        // Initialize a vector to store the count of set bits for each number from 0 to n
+        vector<int> res(n + 1, 0);
+
+        // Iterate from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // Use Brian Kernighan's Algorithm to count set bits
+            res[i] = countSetBits(i);
+        }
+    display(res);
+
+        return res;
+    }
+
 };
 int main() {
 
   Solution solution;
 
-  vector<int> result = solution.countBits(5);
+  vector<int> result = solution.countBitsWithBrianKernighan(5);
 
   return 0;
 }
